a790f738c747c2fdcde0c6ad29c7a2bfe0da5786
sedzen <sedzen@ukr.net>
2011-12-13 19:48:26 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weare::Application.config.secret_token = '0a63986d07b62db7599caa9f96efa95c5ecb41be455c837fa92e4c4d630f2a96646c21bbda205979b42c936c139097d3d04bfdbb6b3c371adf13f2ceab5fa1cc'
